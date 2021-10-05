50f773bde0c52a64d48e99f3c50166433bcdb533
Madison <madivallarino@gmail.com>
2021-10-04 20:41:08 -0400

    basic app stuff i dunno

--- my-app-frontend/src/App.js
+++ my-app-frontend/src/App.js
@@ -1,7 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+
+  const getSubjects = () => {
+    fetch('http://localhost:9292/subjects')
+    .then(r => r.json())
+    .then(subject => console.log(subject))
+  }
+
+  const getSubject = (id) => {
+    fetch(`http://localhost:9292/subject/${id}`)
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+  }
+
+
+  const updateQuestion = (formData, id) => {
+    const {prompt, answer1, answer2, answer3, answer4, correct_answer, subject_id} = formData
+
+  const question = {
+    prompt, 
+    answer1, 
+    answer2,
+    answer3,
+    answer4,
+    correct_answer,
+    subject_id
+  }
+  const configObj = {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(question)
+  }
+  fetch(`http://localhost:9292/question/${id}`, configObj)
+  .then(r => r.json())
+  .then(data => console.log(data))
+  }
+
+  const deleteQuestion = (id) => {
+    fetch(`http://localhost:9292/question/${id}`, {
+      method: 'DELETE'
+    })
+  }
+  // debugger;
   return (
     <div className="App">
       <header className="App-header">
@@ -18,7 +63,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </div> 
   );
 }
 
